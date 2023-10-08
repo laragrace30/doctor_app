74c3dc7c96bbaa4dcf344086ea6caf8b39163f5d
coar14 <m.masong14@gmail.com>
2023-10-08 18:51:25 +0800

    for home screen page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,8 @@
+import 'package:doctor_app/components/doctor_app_grid_menu.dart';
+import 'package:doctor_app/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:google_fonts/google_fonts.dart';
 
+import 'package:doctor_app/components/home_screen_navbar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,69 +10,74 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        toolbarHeight: 100,
-        leading: IconButton(
-          icon: SvgPicture.asset('assets/icons/Icons-Menu-Burger.svg'),
-         onPressed: (){},
-        ),
-        actions: [
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Image.asset(
-                               'assets/images/Mask Group.png',
-                               height: 35,
-                  ),
-                ),
-        ],
-      ),
-      backgroundColor: Colors.white,
       body: SafeArea(
-        child: ListView(
-          padding: const EdgeInsets.all(20),
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                    TextSpan(
-                    text: 'Find',
-                    style: GoogleFonts.roboto(
-                    fontWeight: FontWeight.normal,
-                    color: Colors.black,
-                    fontSize: 36,
-                    ),
-                  ),
-                  TextSpan(
-                  text: ' your doctor',
-                  style: GoogleFonts.roboto(
-                    fontWeight: FontWeight.normal,
-                    color: Colors.grey,
-                    fontSize: 36,
-                        ),
-                      )
-                    ] 
+              children: [
+                const HomeScreenNavbar(),
+                SizedBox(
+                  height: 30,
+                ),
+                RichText(
+                  text: TextSpan(
+                    style: Theme.of(context).textTheme.displayLarge,
+                    children: <TextSpan>[
+                      const TextSpan(
+                        text: 'Find ',
+                      ),
+                      TextSpan(
+                        text: 'your doctor',
+                        style:
+                            Theme.of(context).textTheme.displayLarge!.copyWith(
+                                  color: kGreyColor900,
+                                ),
+                      ),
+                    ],
                   ),
                 ),
                 const SizedBox(
-                  height: 20,
+                  height: 24,
                 ),
                 Container(
-                  height: 55,
-                  padding: const EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    color: Colors.grey.shade200,
-                      )
+                  height: 56,
+                  padding: const EdgeInsets.only(
+                    right: 0,
+                    left: 16,
+                    bottom: 5,
+                    top: 6,
                   ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: kGreyColor500,
+                  ),
+                  child: TextField(
+                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                          color: kBlackColor900,
+                        ),
+                    cursorHeight: 24,
+                    decoration: InputDecoration(
+                      suffixIcon: const Icon(
+                        Icons.search,
+                        color: kBlackColor900,
+                      ),
+                      suffixIconConstraints: const BoxConstraints(
+                        maxHeight: 24,
+                      ),
+                      hintText: 'Search doctor, medicines etc',
+                      hintStyle: Theme.of(context).textTheme.headlineSmall,
+                      contentPadding: const EdgeInsets.only(bottom: 5),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                const DoctorAppGridMenu(),
               ],
-            )
-          ],
-        )
+            ),
+          ),
+        ),
       ),
     );
   }
