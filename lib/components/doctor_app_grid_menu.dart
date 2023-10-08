2755b3f33dd7154e53fe6e1cefbf21f508923f7c
Lara Grace Riconalla <laragrace.riconalla.20@usjr.edu.ph>
2023-10-08 23:49:11 +0800

    update

--- lib/components/doctor_app_grid_menu.dart
+++ lib/components/doctor_app_grid_menu.dart
@@ -7,8 +7,48 @@ class DoctorAppGridMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      
-    );
+    return GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          mainAxisSpacing: 10,
+        ),
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        padding: EdgeInsets.zero,
+        itemCount: doctorMenu.length,
+        itemBuilder: (BuildContext context, index) {
+          return GestureDetector(
+            child: Container(
+              constraints: const BoxConstraints(
+                maxHeight: 81,
+              ),
+              child: Column(children: [
+                Container(
+                  constraints: const BoxConstraints(
+                    minHeight: 56,
+                    minWidth: 56,
+                    maxHeight: 69,
+                    maxWidth: 69,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: Svg('assets/svg/${doctorMenu[index].image}'),
+                    ),
+                  ),
+                ),
+                // const SizedBox(
+                //   height: 4,
+                // ),
+                Expanded(
+                    child: Text(
+                  doctorMenu[index].name,
+                  style: Theme.of(context).textTheme.titleLarge,
+                )),
+              ]),
+            ),
+          );
+        });
   }
 }
